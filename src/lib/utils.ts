dc09965243bfbd54f5f2cbb8dd67f26949c4ce04
최민혁 <nogoso3@gmail.com>
2025-03-03 16:42:03 +0900

    shadcn/ui 추가

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+// src/lib/utils.ts
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
