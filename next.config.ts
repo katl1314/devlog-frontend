0ecfa87c02daa4fb3ae2fee8aa1a62dc4a94ddba
최민혁 <nogoso3@gmail.com>
2025-03-08 14:58:01 +0900

    루트 경로 rewrite 적용

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // rewrite
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/week",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
