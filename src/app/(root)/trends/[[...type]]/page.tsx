14e6ff36a03c9011864af906cb966561b2a20618
최민혁 <nogoso3@gmail.com>
2025-03-09 23:18:26 +0900

    구문 수정

--- src/app/(root)/trends/[[...type]]/page.tsx
+++ src/app/(root)/trends/[[...type]]/page.tsx
@@ -3,7 +3,7 @@ import { notFound } from "next/navigation";
 export default async function Trends({ params }: { params: { type: any } }) {
   // 경로 Validation
   const pages = ["week", "day", "month", "year"];
-  const type = (await params.type?.[0]) ?? "week";
+  const type = (await params).type?.[0] ?? "week";
 
   // week, day, month, year외 경로 접근 시 notFound
   if (!pages.includes(type)) notFound();
