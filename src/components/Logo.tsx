69a3438f6fb09c920579a2d1484b727134942cb8
최민혁 <nogoso3@gmail.com>
2025-03-09 23:18:16 +0900

    로고 선택 시 루트 경로로 이동하도록 수정

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ import style from "./Logo.module.css";
 export default function Logo() {
   return (
     <div className={style.container}>
-      <Link href={"./"} className={style.logo}>
+      <Link href={"/"} className={style.logo}>
         <Image src={"/icon.svg"} alt="로고이미지" width={50} height={50} />
         <span className={style.text}>Moodoo</span>
       </Link>
