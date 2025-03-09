2a488def33b6893270a4ff0c3c588d8e220fc088
최민혁 <nogoso3@gmail.com>
2025-03-09 23:16:44 +0900

    breakpoint 수정

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,15 @@
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    "@tailwindcss/postcss": {
+      screens: {
+        mobile: "320px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
   },
 };
 export default config;
