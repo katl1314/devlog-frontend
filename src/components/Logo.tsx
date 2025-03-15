05abd8e1e8c309c5aca80bb16d42261f162be3c6
최민혁 <nogoso3@gmail.com>
2025-03-15 17:11:48 +0900

    Logo tailwind css 수정

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
-import style from "./Logo.module.css";
 
 export default function Logo() {
   return (
-    <div className={style.container}>
-      <Link href={"/"} className={style.logo}>
+    <div className="flex items-center">
+      <Link href={"/"} className="flex items-center no-underline">
         <Image src={"/icon.svg"} alt="로고이미지" width={50} height={50} />
-        <span className={style.text}>Moodoo</span>
+        <span className="ml-[10px] text-[20px] font-bold text-[#333]">
+          Moodoo
+        </span>
       </Link>
     </div>
   );
