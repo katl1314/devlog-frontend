04505a89c38c8efeacba29700f9b4527a2994118
최민혁 <nogoso3@gmail.com>
2025-03-17 23:57:51 +0900

    Modal 표시되는동안 body스크롤 동작방지

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -19,6 +19,7 @@ export default function Modal({ children }: { children: ReactNode }) {
       modalRef.current?.scrollTo({
         top: 0,
       });
+      document.body.style.overflow = "hidden";
       // 포커스 방지
       modalRef.current?.focus({ preventScroll: true });
     }
@@ -27,13 +28,11 @@ export default function Modal({ children }: { children: ReactNode }) {
   return createPortal(
     <dialog
       ref={modalRef}
-      className="w-[100%] border-none rounded-[5px] my-[10%] mx-auto lg:w-[35%] backdrop:bg-[rgba(0,0,0,0.2)]"
-      onClose={() => {
-        router.back(); // esc키 눌렀을 때 모달 닫힘.
-      }}
+      className="w-[100%] border-none rounded-[5px] my-[60%] mx-auto backdrop:bg-[rgba(0,0,0,0.2)] lg:w-[35%] lg:my-[10%]"
       onClick={(e) => {
         // 모달 클릭 시 뒤로가기 => 뒷 배경
         if ((e.target as HTMLElement).nodeName === "DIALOG") {
+          document.body.style.overflow = "";
           router.back();
         }
       }}
