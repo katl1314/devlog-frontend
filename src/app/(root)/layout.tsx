859fad68fa367b674a2cc67350666e6465147af1
최민혁 <nogoso3@gmail.com>
2025-03-17 22:21:21 +0900

    루트 div my속성 삭제

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -24,7 +24,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   };
 
   return (
-    <div className=" my-[10px] mx-auto">
+    <div className="mx-auto">
       <PageLayout>
         <Header />
         <TabLayout>
