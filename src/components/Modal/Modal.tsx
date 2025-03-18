a7bea19fb167b1d493f70384511bec0fa1066f54
최민혁 <nogoso3@gmail.com>
2025-03-18 23:33:15 +0900

    카카오 로그인 버튼 생성

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,18 +2,18 @@
 
 import { ReactNode, useEffect, useRef, MouseEventHandler } from "react";
 import { createPortal } from "react-dom";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 interface Modal {
   children: ReactNode;
-  afterCloseModal?: () => void;
+  afterCloseModal?: (target: EventTarget) => void;
 }
 
 // 모달은 React의 Portal을 사용한다.
 export default function Modal({ children, afterCloseModal }: Modal) {
   // createPortal을 통해 모달로 보여줄 컴포넌트와, 위치 엘리먼트를 전달한다.
   const modalRef = useRef<HTMLDialogElement>(null);
-  const router = useRouter();
+  // const router = useRouter();
   useEffect(() => {
     // 화면에 마운트 되는 시점에 실행한다.
     if (!modalRef.current?.open) {
@@ -29,10 +29,12 @@ export default function Modal({ children, afterCloseModal }: Modal) {
   }, []);
 
   const handleClose: MouseEventHandler<HTMLDialogElement> = (e) => {
-    if ((e.target as HTMLElement).nodeName === "DIALOG") {
-      router.back();
-    }
-    afterCloseModal?.();
+    // 로직 체크 필요... 인터셉트 라우팅 여부 확인 필
+    // if ((e.target as HTMLElement).nodeName === "DIALOG") {
+    //   router.back();
+    // }
+    modalRef.current?.close();
+    afterCloseModal?.(e.target);
   };
 
   return createPortal(
