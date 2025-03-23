55fc2c893a8aa3928849ecec32ff6e3e0ade2271
최민혁 <nogoso3@gmail.com>
2025-03-23 21:55:18 +0900

    주석 추가

--- src/app/(root)/[tab]/page.tsx
+++ src/app/(root)/[tab]/page.tsx
@@ -3,7 +3,8 @@ import { Suspense } from 'react';
 import PostCardSkeleton from '@/components/Skeleton/PostCardSkeleton';
 import PostCardList from '@/components/PostCardList';
 
-export const dynamicParams = false; // Static Side Generate형태로 빌드된다.
+// generatedStaticParams로 생성되지 않은 정적 페이지에 접근 시 제어한다.
+export const dynamicParams = false; // false 시 404페이지를 발생한다.
 
 interface Page {
 	params: Promise<{ tab: string }>;
@@ -13,8 +14,9 @@ export async function generateStaticParams() {
 	try {
 		const url = 'http://localhost:3001/tabs';
 		const res = await fetch(url, { cache: 'force-cache' });
-		const data = await res.json();
-		return data;
+
+		if (!res.ok) throw new Error('데이터 조회 중 에러가 발생하였습니다.');
+		return await res.json();
 	} catch (err) {
 		console.error(err);
 	}
@@ -22,7 +24,6 @@ export async function generateStaticParams() {
 
 export default async function Page({ params }: Page) {
 	const { tab } = await params;
-
 	return (
 		<CardLayout>
 			<Suspense fallback={<PostCardFallback />}>
