b9605726e206f895cb537a30f9d566d905dc49c7
최민혁 <nogoso3@gmail.com>
2025-03-26 23:45:18 +0900

    네이버 로그인 이미지 추가

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,6 @@ export default function Logo() {
 	return (
 		<div className="flex items-center">
 			<Link href={'/'} className="flex items-center no-underline">
-				{/* <Image src={"/icon.svg"} alt="로고이미지" width={50} height={50} /> */}
 				<span className="text-[20px] font-bold text-[#333]">Devs.log</span>
 			</Link>
 		</div>
