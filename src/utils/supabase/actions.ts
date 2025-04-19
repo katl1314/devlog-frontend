c853a298c32949231761ead329e627291f2b4421
최민혁 <nogoso3@gmail.com>
2025-04-19 23:46:15 +0900

    로그인 후 callback에서 next속성 처리

--- src/utils/supabase/actions.ts
+++ src/utils/supabase/actions.ts
@@ -5,11 +5,12 @@ import { createClientByServer } from './server';
 import { redirect } from 'next/navigation';
 
 // Provider => github | google | apple | kakao ...
-const signInWith = (provider: Provider) => async () => {
+const signInWith = (provider: Provider) => async (formData: FormData) => {
 	const supabase = await createClientByServer();
 	// 위 경로를 직접 구현해야함. 위치에 page.tsx가 아닌 routes.ts를 생성해야함.
 	// 로그인 결과를 /auth/callback으로 전달한다.
-	const auth_callback_url = `${process.env.SITE_URL}/auth/callback`; // /auth/callback/route.ts에서 request 핸들링
+	const next = formData.get('next');
+	const auth_callback_url = `${process.env.SITE_URL}/auth/callback?next=${next}`; // /auth/callback/route.ts에서 request 핸들링
 
 	const { data, error } = await supabase.auth.signInWithOAuth({
 		provider,
