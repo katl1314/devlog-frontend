94e766f721b89747f519d4107e4a9fa200b5d61b
최민혁 <nogoso3@gmail.com>
2025-04-26 17:59:41 +0900

    버튼 수정

--- src/components/Profile/LoginButton.tsx
+++ src/components/Profile/LoginButton.tsx
@@ -4,6 +4,7 @@ import { Dropdown } from '../Dropdown';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import { Button } from '../ui/button';
 import { createClientByBrowser } from '@/utils/supabase/client';
+import { redirect } from 'next/navigation';
 
 export default function LoginButton() {
 	const { logout, avatar_url } = useProfile();
@@ -15,9 +16,16 @@ export default function LoginButton() {
 			window.location.reload();
 		});
 	};
+
+	const handleWrite = () => {
+		redirect('/write');
+	};
+
 	return (
 		<>
-			<Button className="flex items-center font-bold cursor-pointer">글 쓰기</Button>
+			<Button className="flex items-center font-bold cursor-pointer border-b" variant={'outline'} onClick={handleWrite}>
+				새 글 작성
+			</Button>
 			<Dropdown handleLogOut={handleLogout}>
 				<Avatar className="cursor-pointer">
 					<AvatarImage src={avatar_url ?? 'https://github.com/shadcn.png'} />
