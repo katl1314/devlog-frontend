61e951a6c04c2926e587b18e46f8e195c7680031
최민혁 <nogoso3@gmail.com>
2025-05-02 00:01:05 +0900

    editor 수정

--- src/components/CustomEditor.tsx
+++ src/components/CustomEditor.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-
+// 컴포넌트 정리 합시다.
 import { ParagraphNode, TextNode } from 'lexical';
 import { HeadingNode, QuoteNode } from '@lexical/rich-text';
 import { InitialConfigType, LexicalComposer } from '@lexical/react/LexicalComposer';
@@ -12,7 +12,6 @@ import { TooltipProvider } from '@/components/ui/tooltip';
 import { editorTheme } from '@/components/editor/themes/editor-theme';
 import { ContentEditable } from '@/components/editor/editor-ui/content-editable';
 import { ToolbarPlugin } from '@/components/editor/plugins/toolbar/toolbar-plugin';
-import { HistoryToolbarPlugin } from '@/components/editor/plugins/toolbar/history-toolbar-plugin';
 import { BlockFormatDropDown } from './editor/plugins/toolbar/block-format-toolbar-plugin';
 import { FormatParagraph } from './editor/plugins/toolbar/block-format/format-paragraph';
 import { FormatBulletedList } from './editor/plugins/toolbar/block-format/format-bulleted-list';
@@ -20,26 +19,62 @@ import { FormatCheckList } from './editor/plugins/toolbar/block-format/format-ch
 import { FormatHeading } from './editor/plugins/toolbar/block-format/format-heading';
 import { FormatNumberedList } from './editor/plugins/toolbar/block-format/format-numbered-list';
 import { FormatQuote } from './editor/plugins/toolbar/block-format/format-quote';
+import { FontFormatToolbarPlugin } from '@/components/editor/plugins/toolbar/font-format-toolbar-plugin';
+import { FontBackgroundToolbarPlugin } from '@/components/editor/plugins/toolbar/font-background-toolbar-plugin';
+import { FontColorToolbarPlugin } from '@/components/editor/plugins/toolbar/font-color-toolbar-plugin';
+import { ElementFormatToolbarPlugin } from '@/components/editor/plugins/toolbar/element-format-toolbar-plugin';
+import { ClickableLinkPlugin } from '@lexical/react/LexicalClickableLinkPlugin';
+import { MarkdownShortcutPlugin } from '@lexical/react/LexicalMarkdownShortcutPlugin';
+import { CodeNode } from '@lexical/code';
+import { ListNode, ListItemNode } from '@lexical/list';
+import { HorizontalRuleNode } from '@lexical/react/LexicalHorizontalRuleNode';
+import { LinkNode, AutoLinkNode } from '@lexical/link';
+import { cn } from '@/lib/utils';
 
 const editorConfig: InitialConfigType = {
 	namespace: 'Editor',
 	theme: editorTheme,
-	nodes: [HeadingNode, ParagraphNode, TextNode, QuoteNode],
+	nodes: [
+		HeadingNode,
+		QuoteNode,
+		CodeNode,
+		ListNode,
+		ListItemNode,
+		ParagraphNode,
+		TextNode,
+		HorizontalRuleNode,
+		LinkNode,
+		AutoLinkNode
+	],
 	onError: (error: Error) => {
 		console.error(error);
 	}
 };
 
-export default function CustomEditor() {
+interface Editor {
+	useToolbar?: boolean;
+	size?: keyof typeof editorSize;
+	useMarkdown?: boolean;
+}
+
+const editorSize = {
+	sm: 'h-[70px]',
+	base: 'h-[200px]',
+	lg: 'h-[300px]',
+	xlg: 'h-[calc(100vh-300px)]',
+	xxlg: 'h-[calc(100vh-200px)]'
+};
+
+export default function CustomEditor(props: Editor) {
 	return (
-		<div className="w-full overflow-hidden rounded-lg border bg-background shadow">
+		<div className="$w-full overflow-hidden rounded-lg border bg-background shadow">
 			<LexicalComposer
 				initialConfig={{
 					...editorConfig
 				}}
 			>
 				<TooltipProvider>
-					<Plugins />
+					<Plugins {...props} />
 				</TooltipProvider>
 			</LexicalComposer>
 		</div>
@@ -48,9 +83,8 @@ export default function CustomEditor() {
 
 const placeholder = 'Start typing...';
 
-export function Plugins() {
-	const [floatingAnchorElem, setFloatingAnchorElem] = useState<HTMLDivElement | null>(null);
-	console.log(floatingAnchorElem);
+export function Plugins({ useToolbar = true, useMarkdown = true, size = 'base' }: Editor) {
+	const [, setFloatingAnchorElem] = useState<HTMLDivElement | null>(null);
 	const onRef = (_floatingAnchorElem: HTMLDivElement) => {
 		if (_floatingAnchorElem !== null) {
 			setFloatingAnchorElem(_floatingAnchorElem);
@@ -59,22 +93,30 @@ export function Plugins() {
 
 	return (
 		<div className="relative">
-			{/* toolbar plugins */}
-			<ToolbarPlugin>
-				{() => (
-					<div className="vertical-align-middle sticky top-0 z-10 flex gap-2 overflow-auto border-b p-1">
-						<HistoryToolbarPlugin />
-						<BlockFormatDropDown>
-							<FormatParagraph />
-							<FormatHeading levels={['h1', 'h2', 'h3']} />
-							<FormatNumberedList />
-							<FormatBulletedList />
-							<FormatCheckList />
-							<FormatQuote />
-						</BlockFormatDropDown>
-					</div>
-				)}
-			</ToolbarPlugin>
+			{useToolbar && (
+				<ToolbarPlugin>
+					{() => (
+						<div className="vertical-align-middle sticky top-0 z-10 flex gap-2 overflow-auto border-b p-1">
+							<BlockFormatDropDown>
+								<FormatParagraph />
+								<FormatHeading levels={['h1', 'h2', 'h3']} />
+								<FormatNumberedList />
+								<FormatBulletedList />
+								<FormatCheckList />
+								<FormatQuote />
+							</BlockFormatDropDown>
+							<FontFormatToolbarPlugin format="bold" />
+							<FontFormatToolbarPlugin format="italic" />
+							<FontFormatToolbarPlugin format="underline" />
+							<FontFormatToolbarPlugin format="strikethrough" />
+							<FontColorToolbarPlugin />
+							<FontBackgroundToolbarPlugin />
+							<ElementFormatToolbarPlugin />
+							<ClickableLinkPlugin />
+						</div>
+					)}
+				</ToolbarPlugin>
+			)}
 
 			<div className="relative">
 				<RichTextPlugin
@@ -83,7 +125,10 @@ export function Plugins() {
 							<div className="" ref={onRef}>
 								<ContentEditable
 									placeholder={placeholder}
-									className="ContentEditable__root relative block min-h-72 overflow-auto min-h-full px-8 py-4 focus:outline-none h-72"
+									className={cn(
+										'ContentEditable__root relative block min-h-72 overflow-auto min-h-full px-8 py-4 focus:outline-none',
+										editorSize[size]
+									)}
 								/>
 							</div>
 						</div>
@@ -91,7 +136,7 @@ export function Plugins() {
 					ErrorBoundary={LexicalErrorBoundary}
 				/>
 				<HistoryPlugin />
-				{/* rest of the plugins */}
+				{useMarkdown && <MarkdownShortcutPlugin />}
 			</div>
 		</div>
 	);
--- src/components/PostEditor.tsx
+++ src/components/PostEditor.tsx
@@ -9,15 +9,18 @@ export default function PostEditor() {
 	// 모바일인지 아닌지 확인은 해상도를 통해서...
 
 	return (
-		<div className="">
-			<div>
-				<CustomEditor />
+		<div className="flex flex-col mt-4 justify-between">
+			<div className="flex flex-col gap-2 flex-1">
+				{/* 제목 */}
+				<CustomEditor useToolbar={false} size="sm" useMarkdown={false} />
+				{/* 본문 */}
+				<CustomEditor size="xlg" />
 			</div>
-			<div className="w-full flex justify-between items-center">
+			<div className="w-full flex justify-between items-center mt-4">
 				<div className="flex gap-4">
-					<Link href="/" className="flex items-center p-2 gap-2 group hover:bg-black">
+					<Link href="/" className="flex items-center p-2 gap-2 group hover:bg-black transition-colors duration-300">
 						<FiArrowLeft size={28} className="group-hover:stroke-[white] cursor-pointer" />
-						<Label className="group-hover:text-white text-lg font-bold cursor-pointer">이전</Label>
+						<Label className="group-hover:text-white lg:text-lg font-bold cursor-pointer">이전</Label>
 					</Link>
 				</div>
 				<div className="flex gap-4">
