0790a47c45361a304ea1a99b70a44cab872e21ea
최민혁 <nogoso3@gmail.com>
2026-02-09 22:15:51 +0900

    데이터 패칭 커스텀훅 개발

--- src/components/Post/PostCardList.tsx
+++ src/components/Post/PostCardList.tsx
@@ -1,11 +1,9 @@
 'use client';
 
-import {useSuspenseInfiniteQuery } from '@tanstack/react-query';
-import { FetchPostsResponse } from '@/types/type';
 import { postService } from '@/services/post.service';
 import PostCard from '@/components/Post/PostCard';
 import CardLayout from '../layout/CardLayout';
-import { useCallback, useRef } from 'react';
+import useFetch from '@/hooks/fetch';
 
 const fetchPosts: any = async ({ cursor = 0 }) => {
 	const posts = await postService.getList(cursor);
@@ -13,32 +11,12 @@ const fetchPosts: any = async ({ cursor = 0 }) => {
 };
 
 export default function PostCardList() {
-	const { data, fetchNextPage, hasNextPage } =
-		useSuspenseInfiniteQuery<FetchPostsResponse>({
-			queryKey: ['posts'], // 쿼리의 고유 키, 캐싱/리패칭 기준 일반적으로 배열 (어떤 게시물의 쿼리인가)
-			getNextPageParam: (lastPage) => lastPage.nextCursor ? lastPage.nextCursor - 1 :  undefined,
-			queryFn:  ({ pageParam = 0 }) => fetchPosts({ cursor: pageParam as number }),
-			initialPageParam: 0, // 초기값
-		});
-
-	const observer = useRef<IntersectionObserver>(null);
-
-	// 콜백함수 메모이제이션
-	const lastPostRef = useCallback(
-		(node: HTMLDivElement) => {
-			if (observer.current) observer.current.disconnect();
-
-			if (node && hasNextPage) {
-				const observeCallback: IntersectionObserverCallback = ([entry]) => {
-					// getNextPageParam
-					entry.isIntersecting && fetchNextPage();
-				};
-				observer.current = new IntersectionObserver(observeCallback);
-				observer.current.observe(node);
-			}
-		},
-		[hasNextPage, fetchNextPage]
-	);
+	const { data, lastPostRef } = useFetch({
+		initialPageParam: 0,
+		queryKey: ['posts'],
+		getNextPageParam: (lastPage) => lastPage.nextCursor ? lastPage.nextCursor - 1 :  undefined,
+		queryFn:  ({ pageParam = 0 }) => fetchPosts({ cursor: pageParam }),
+	})
 
 	return (
 		<CardLayout>
---
+++ src/hooks/fetch.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { GetNextPageParamFunction, useSuspenseInfiniteQuery, QueryFunction } from '@tanstack/react-query';
+import { FetchPostsResponse } from '@/types/type';
+import { useCallback, useRef } from 'react';
+
+type IPagingComponent = {
+  queryKey: string[] | string, // 쿼리의 고유 키, 캐싱/리패칭 기준 일반적으로 배열 (어떤 게시물의 쿼리인가)
+  initialPageParam: number, // 페이징 초기값
+  queryFn: QueryFunction<FetchPostsResponse, readonly unknown[], unknown>, // 데이터 패칭 함수
+  getNextPageParam: GetNextPageParamFunction<unknown, FetchPostsResponse>; // 다음 페이지 정보 반환하는 함수
+} & React.PropsWithChildren;
+
+
+export default function useFetch({ queryKey, initialPageParam,  queryFn, getNextPageParam}: IPagingComponent) {
+  const { data, fetchNextPage, hasNextPage } =
+    useSuspenseInfiniteQuery<FetchPostsResponse>({
+      queryKey: Array.isArray(queryKey) ? queryKey : [queryKey],
+      initialPageParam,
+      getNextPageParam,
+      queryFn,
+    });
+
+  const observer = useRef<IntersectionObserver>(null);
+
+  // 콜백함수 메모이제이션
+  const lastPostRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (observer.current) observer.current.disconnect();
+
+      if (node && hasNextPage) {
+        const observeCallback: IntersectionObserverCallback = ([entry]) => {
+          // getNextPageParam
+          entry.isIntersecting && fetchNextPage();
+        };
+        observer.current = new IntersectionObserver(observeCallback);
+        observer.current.observe(node);
+      }
+    },
+    [hasNextPage, fetchNextPage]
+  );
+
+  return {
+    lastPostRef,
+    data,
+  }
+}
