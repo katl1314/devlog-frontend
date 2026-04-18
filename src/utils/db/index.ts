1491589d7e10b3229a1ce9c0c32f48b270aaa0a7
최민혁 <nogoso3@gmail.com>
2026-04-18 13:34:32 +0900

    style(db): Prettier 포맷팅 적용

--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -1,32 +1,33 @@
 import { isEmpty } from '@/utils';
 
 type FetchOptions = RequestInit & {
-	params?: Record<string, string>
+	params?: Record<string, string>;
 };
 
-export const apiClient = async(endpoint: string, options: FetchOptions = {}) => {
-
+export const apiClient = async (
+	endpoint: string,
+	options: FetchOptions = {}
+) => {
 	const { params, headers, ...other } = options;
 	// URL 파라미터 조립
-	const queryString = !isEmpty(params)
-		? `?${new URLSearchParams(params)}`
-		: '';
+	const queryString = !isEmpty(params) ? `?${new URLSearchParams(params)}` : '';
 	// 서버 사이드는 Docker 내부 주소, 클라이언트 사이드는 nginx 경유
-	const baseUrl = typeof window === 'undefined'
-		? process.env.SERVER_URL
-		: process.env.NEXT_PUBLIC_SERVER_URL;
+	const baseUrl =
+		typeof window === 'undefined'
+			? process.env.SERVER_URL
+			: process.env.NEXT_PUBLIC_SERVER_URL;
 	// URL 설정
 	const url = `${baseUrl}${endpoint}${queryString}`;
 	// 헤더 설정
 	const defaultHeaders = {
 		'Content-Type': 'application/json',
 		...headers
-	}
+	};
 
 	// 요청
 	const res = await fetch(url, {
 		...other,
-		headers: defaultHeaders,
+		headers: defaultHeaders
 	});
 
 	// 요청 실패 시
@@ -36,4 +37,4 @@ export const apiClient = async(endpoint: string, options: FetchOptions = {}) => 
 	}
 
 	return await res.json();
-}
+};
