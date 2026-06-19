70d0eda5c4ba700f11d639f16387a8d97aa81509
nogoso3 <nogoso3@gmail.com>
2026-06-19 22:23:19 +0900

    css 개선

--- src/app/(blog)/user/[userId]/[path]/layout.tsx
+++ src/app/(blog)/user/[userId]/[path]/layout.tsx
@@ -1,7 +1,3 @@
-export default async function Layout({
-	children
-}: {
-	children: React.ReactNode;
-}) {
-	return <main className="relative py-4">{children}</main>;
+export default async function Layout({ children }: { children: React.ReactNode }) {
+	return <main className="relative">{children}</main>;
 }
